7df427a221f8a3957bd1781d563758764439c1d4
Damian Kubiak <kubiak.damian96@gmail.com>
2019-03-04 19:45:22 +0100

    project creation

---
+++ src/main/java/pl/qbsapps/checkIt/CheckItApplication.java
@@ -0,0 +1,13 @@
+package pl.qbsapps.checkIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckItApplication.class, args);
+	}
+
+}
